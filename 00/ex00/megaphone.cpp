4323cc4600382846605a2c374faf6b27236bb8ba
Yohann Gaiffier <ygaiffie@2C4.42angouleme.fr>
2024-11-12 12:50:56 +0100

    remove algorythm method

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -3,33 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 15:02:23 by aska              #+#    #+#             */
-/*   Updated: 2024/11/01 11:23:37 by aska             ###   ########.fr       */
+/*   Updated: 2024/11/12 12:34:22 by ygaiffie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <cctype>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	if (ac == 1)
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	}
 	else
 	{
 		for (int i = 1; i < ac; ++i)
 		{
-			std::string word(av[i]);
-			std::transform(word.begin(), word.end(), word.begin(), ::toupper);
-			std::cout << word << " ";
+			std::string str = av[i];
+			for (int j = 0; av[i][j]; ++j)
+				std::cout << (char)toupper(str[j]);
 		}
 		std::cout << std::endl;
 	}
-	return 0;
+	return (0);
 }
