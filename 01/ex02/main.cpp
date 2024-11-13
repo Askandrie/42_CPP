fbc158a9a27974ad99c1dd23e1e00a36956a1985
aska <kamsheend@gmail.com>
2024-11-13 23:05:41 +0100

    begin ex03

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -3,19 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:08:03 by ygaiffie          #+#    #+#             */
-/*   Updated: 2024/11/13 17:17:10 by ygaiffie         ###   ########.fr       */
+/*   Updated: 2024/11/13 22:25:11 by aska             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
+#include <iostream>
 
 int main()
 {
 	std::string str = "HI THIS IS BRAIN";
-	const char *stringPTR = str.c_str;
-	void *stringREF = &str;
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
+	std::cout << "Address of str\t\t: " << &str << std::endl;
+	std::cout << "Address of stringPTR\t: " << &stringPTR << std::endl;
+	std::cout << "Address of stringREF\t: " << &stringREF << std::endl;
+
+	std::cout << "Value of str\t\t: " << str << std::endl;
+	std::cout << "Value of stringPTR\t: " << stringPTR << std::endl;
+	std::cout << "Value of stringREF\t: " << stringREF << std::endl;
+
+	return 0;
 }
