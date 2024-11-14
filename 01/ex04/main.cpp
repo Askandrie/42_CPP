37362b644679243bfbbee27aaad157bbcbd37a16
Yohann Gaiffier <ygaiffie@2C4.42angouleme.fr>
2024-11-14 16:48:59 +0100

    finish ex04 and begin 05

---
+++ 01/ex04/main.cpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 13:14:10 by ygaiffie          #+#    #+#             */
+/*   Updated: 2024/11/14 16:20:38 by ygaiffie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int printUsage()
+{
+	std::cout << "Usage: <Filename> <find STR> <replace STR>\n"
+			  << std::endl;
+	return 1;
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+		return printUsage();
+	std::ifstream file(av[1]);
+	if (file.is_open())
+	{
+		std::string filenameReplace = av[1], s1 = av[2], s2 = av[3], line;
+		filenameReplace += ".replace";
+		std::ofstream fileReplace(filenameReplace.c_str());
+		while (std::getline(file, line))
+		{
+			size_t pos = line.find(s1);
+			while (pos != line.npos)
+			{
+				if (pos != line.npos)
+				{
+					line.erase(pos, s1.length());
+					line.insert(pos, s2);
+				}
+				pos = line.find(s1);
+			}
+			fileReplace << line << std::endl;
+		}
+		file.close();
+		std::cout << "DONE" << av[1] << std::endl;
+	}
+	else
+	{
+		std::cout << "Failed to open file" << av[1] << std::endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ 01/ex05/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 16:32:40 by ygaiffie          #+#    #+#             */
+/*   Updated: 2024/11/14 16:43:51 by ygaiffie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
+
+	private:
+		void debug();
+		void info();
+		void warning();
+		void error();
+};
+
+#endif
