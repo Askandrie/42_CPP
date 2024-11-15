d6b6e635b6f9031c39b1d70a43ba0a439b5d98e1
Yohann Gaiffier <ygaiffie@2B3.42angouleme.fr>
2024-11-15 16:23:00 +0100

    finish 01 and change compiler on all Makefile

--- 01/ex06/Harl.cpp
+++ 01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ygaiffie <ygaiffie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:40:13 by ygaiffie          #+#    #+#             */
-/*   Updated: 2024/11/15 13:54:15 by ygaiffie         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:09:38 by ygaiffie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,46 @@ Harl::~Harl() {}
 
 void Harl::debug()
 {
-	std::cout << BGRN << DEBUG_MSG << std::endl;
+	std::cout << BGRN << "[ DEBUG ]\n" << DEBUG_MSG << std::endl << std::endl;
 }
 
 void Harl::info()
 {
-	std::cout << BCYN << INFO_MSG << std::endl;
+	std::cout << BCYN << "[ INFO ]\n" << INFO_MSG << std::endl << std::endl;
 }
 
 void Harl::warning()
 {
-	std::cout << BYEL << WARNING_MSG << std::endl;
+	std::cout << BYEL << "[ WARNING ]\n" << WARNING_MSG << std::endl << std::endl;
 }
 
 void Harl::error()
 {
-	std::cout << BRED << ERROR_MSG << std::endl;
+	std::cout << BRED << "[ ERROR ]\n" << ERROR_MSG << std::endl << std::endl;
 }
 
 void Harl::complain(std::string level)
 {
-	ptrFunc tabfunction[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	// ptrFunc tabfunction[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	std::string tablevel[] = {"DEBUG", "INFO" ,"WARNING" ,"ERROR"};
+	int i = 0;
 
-	for (int i = 0; i < 4; i++)
+	while (level != tablevel[i])
+		i++;
+	switch (i)
 	{
-		if (level == tablevel[i])
-		{
-			(this->*tabfunction[i])();
-			return ;
-		}
+		default:
+			std::cout << "INVALID LEVEL" << std::endl;
+			break;
+		case 0:
+			this->debug();
+		case 1:
+			this->info();
+		case 2:
+			this->warning();
+		case 3:
+			this->error();
 	}
 
-	std::cout << "INVALID LEVEL" << std::endl;
+	
 }
