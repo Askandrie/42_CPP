00e28ba524b574977852cd2f2c03a2818b5eabef
Aska <106731287+Askandrie@users.noreply.github.com>
2025-03-05 03:24:15 +0100

    continue Cure.cpp

--- 04/ex04/Cure.cpp
+++ 04/ex04/Cure.cpp
@@ -6,7 +6,7 @@
 /*   By: aska <aska@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 02:19:12 by aska              #+#    #+#             */
-/*   Updated: 2025/03/05 02:39:17 by aska             ###   ########.fr       */
+/*   Updated: 2025/03/05 03:23:39 by aska             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,26 @@
 Cure::Cure() : AMateria("cure")
 {
     std::cout << MAG << "Cure" << RESET << "\tdefault constructor called" << std::endl;
+}
+Cure::Cure(Cure const & ori) : AMateria(ori)
+{
+    std::cout << MAG << "Cure" << RESET << "\tcopy constructor called" << std::endl;
+}
+Cure::~Cure()
+{
+    std::cout << MAG << "Cure" << RESET << "\tdestructor called" << std::endl;
+}
+Cure &Cure::operator=(Cure const & ori)
+{
+    if (this != &ori)
+        this->_type = ori._type;
+    return *this;
+}
+AMateria* Cure::clone() const
+{
+    return new Cure(*this);
+}
+void Cure::use(ICharacter& target)
+{
+    std::cout << MAG << "* heals " << target.getName() << "'s wounds *" << RESET << std::endl;
 }
